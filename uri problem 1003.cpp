28f67509518c0f02450e27271d99b012a631954f
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-04-04 09:09:10 +0600

    Create uri problem 1003.cpp

---
+++ uri problem 1003.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int A=30,B=10,SOMA;
+
+   SOMA=A+B;
+
+   cout <<"SOMA = "<<SOMA;// no use of %d//endl here use for printing new line
+
+   return 0;
+}
