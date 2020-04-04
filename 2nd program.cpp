fc0dc2eafa60bfdc92de8f6dd7a0068827d83690
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-04-04 07:59:50 +0600

    Create 2nd program.cpp

---
+++ 2nd program.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+// here we use conio.h(header file) for using the getch()[function]
+#include<conio.h>
+using namespace std;//international standard
+int main()//program is starting from here
+{//endl is a function
+//endl using for given new line in code
+//but remember you have to must use "" (in the print segment of screen) the endl then again <<"" and lastly (;)
+//cout using here for instead of printf( c programming)
+//count "Bangladesh"<<endl <<"shuvo"<<endl<<"2017";
+cout << "shuvo"<< endl <<"shuvo"<< endl <<"polash";
+//getch form getting the chracter from keyboard
+getch();
+}
