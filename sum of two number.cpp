7b729e792e1263c47aff7f1fe26139fb5b98c03d
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-04-04 08:39:22 +0600

    Create sum of two number.cpp

---
+++ sum of two number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a=1,b=2;//Initializing the two variable
+    int sum=a+b;// storing the sum of them
+    cout<< "the sum is "<<sum;//No use of %d here(its good for me)<<[sum]
+    getch();
+
+}
