a1ce5229e965da32e837ced67ec18679004dccc2
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-04-07 22:21:41 +0600

    Create while loop.c

---
+++ while loop.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+
+int
+main ()
+{
+  int a, n, i = 1;//i initilaize
+  while (i < 5)//only condition in the while
+    {
+      // i++;
+      printf ("Hello World\n");//task
+      i++;//increment of the task
+      //retrun to the condition
+      //if ccondition is true then  task will be again happend
+    }
+  return 0;
+}
